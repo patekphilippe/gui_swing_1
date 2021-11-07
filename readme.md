58a9760b3760073ab3262c5a11009f9cec1349d8
Tomasz Babczyński <tomasz.babczynski@pwr.edu.pl>
2021-11-07 15:00:27 +0100

    dodane atrybuty i metody do klasy MyPanel

--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
 0. program "hello" - konsolowy
-1. puste okno JFrame
+1. puste okno _JFrame_
 2. dodany pusty panel
 3. dodany własny panel przeznaczony do rysowania
-4. dodana klasa Sprajt (abstrakcyjna) i jej realizacja Pilka - element, który będzie rysowany
+4. dodana klasa _Sprajt_ (abstrakcyjna) i jej realizacja _Pilka_ - element, który będzie rysowany
+5. dodane atrybuty i metody do klasy _MyPanel_
--- src/tb/soft/MyPanel.java
+++ src/tb/soft/MyPanel.java
@@ -5,6 +5,36 @@ import javax.swing.border.LineBorder;
 import java.awt.*;
 
 public class MyPanel extends JPanel {
+	private boolean rysuj = true;
+	Sprajt sp = null;
+
+	/**
+	 * @return if painting is active
+	 */
+	public boolean isRysuj() {
+		return rysuj;
+	}
+
+	/**
+	 * @param rysuj to paint or not to paint
+	 */
+	public void setRysuj(boolean rysuj) {
+		this.rysuj = rysuj;
+	}
+
+	/**
+	 * @return if a sprite is added
+	 */
+	public boolean hasSprajt() {
+		return sp != null;
+	}
+
+	/**
+	 * @param spr sprite to be added
+	 */
+	public void addSprajt(Sprajt spr) {
+		this.sp = spr;
+	}
 
 	/**
 	 * Create the panel.
@@ -13,6 +43,23 @@ public class MyPanel extends JPanel {
 		setBorder(new LineBorder(new Color(0, 0, 0), 4, true));
 		setBackground(Color.ORANGE);
 		setLayout(null);
-		setCursor(Cursor.getPredefinedCursor(Cursor.CROSSHAIR_CURSOR));
+
+	}
+
+	/* (non-Javadoc)
+	 * @see javax.swing.JComponent#paintComponent(java.awt.Graphics)
+	 */
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2d = (Graphics2D) g.create();
+		try {
+			if(! isRysuj()) return;
+			if(sp == null) return;
+			g2d.setColor(Color.BLUE);
+			sp.draw(g2d);
+		} finally {
+			g2d.dispose();
+		}
 	}
 }
